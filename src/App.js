6e7254328f699d7ee5e166dc88e5687996f476b7
Bharath Boyapati <91449519+Bharath1720@users.noreply.github.com>
2024-09-09 20:11:02 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import WeatherDashboard from "./components/WeatherDashboard";
+
+const App = () => {
+  return (
+    <div className="bg-red-400 w-screen h-screen">
+      <WeatherDashboard />
+    </div>
+  );
+};
+
+export default App;
